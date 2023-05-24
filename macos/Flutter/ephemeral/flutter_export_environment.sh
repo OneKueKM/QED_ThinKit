c58f641f73213398e1bd58b8d4ddee72014e37de
OneKueKM <onekuemn22@gmail.com>
2023-05-25 00:03:07 +0900

    23.05.25 Commit

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/kmin/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/kmin/Downloads/thin_kit v6.0.3"
+export "FLUTTER_APPLICATION_PATH=/Users/kmin/Desktop/QED_ThinKit"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"

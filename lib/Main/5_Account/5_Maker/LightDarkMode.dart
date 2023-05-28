6851f8f447f0fafb8a443a0665c8c665a826cfcb
OneKueKM <onekuemn22@gmail.com>
2023-05-28 14:48:46 +0900

    DarkMode Icon Fixed

--- lib/Main/5_Account/5_Maker/LightDarkMode.dart
+++ lib/Main/5_Account/5_Maker/LightDarkMode.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:tabler_icons/tabler_icons.dart';
+import 'package:animated_icon_button/animated_icon_button.dart';
 
 class LightDarkMode extends StatefulWidget {
   const LightDarkMode({super.key});
@@ -12,15 +13,25 @@ class LightDarkMode extends StatefulWidget {
 class _LightDarkModeState extends State<LightDarkMode> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 30,
-      width: 50,
-      child: IconButton(
-        icon: const Icon(TablerIcons.moon_stars),
-        color: Colors.black,
-        iconSize: 22,
-        onPressed: () {},
-      ),
-    );
+    return InkWell(
+        splashColor: Colors.transparent, // 터치 효과의 색상을 투명으로 설정
+        highlightColor: Colors.transparent, // 터치 효과의 하이라이트 색상을 투명으로 설정
+        enableFeedback: false,
+        onTap: () {
+          // 클릭 이벤트 처리
+        },
+        child: AnimatedIconButton(
+          duration: const Duration(milliseconds: 500),
+          onPressed: () => print('all icons pressed'),
+          icons: [
+            AnimatedIconItem(
+              icon: Icon(TablerIcons.moon_filled, color: Colors.grey, size: 24),
+              onPressed: () => print('add pressed'),
+            ),
+            AnimatedIconItem(
+              icon: Icon(TablerIcons.sun_filled, color: Colors.grey, size: 24),
+            ),
+          ],
+        ));
   }
 }
